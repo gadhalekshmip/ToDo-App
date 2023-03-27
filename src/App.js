25cf474515f54f7e9904439b4328459176ecf966
Gadha lekshmi p <gadha@Gadhalekshmi.com>
2023-03-28 01:30:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Todo from './components/Todoapp/Todo';
+
+function App() {
+return (
+  <div className='App'>
+    <Todo />
+  </div>
+    
+  );
+};
+
+export default App;
---
+++ src/components/Todoapp/Todo.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import AddTask from './AddTask';
+import ListTask from './ListTask';
+import "./Todo.css";
+
+const Todo = () => {
+    const[tasks,setTask] =useState([]);
+
+    useEffect(()=>{
+        document.title=`You have ${tasks.length} pending task(s)`
+    });
+
+    const addTask=(title)=>{
+        const newTask = [...tasks,{title}]
+        setTask(newTask);
+    };
+    const removeTask=( index)=>{
+        const newTask=[...tasks]
+        newTask.splice(index,1);
+        setTask(newTask);
+    };
+
+  return (
+    <>
+        <div className='todo-container'>
+            <div className='header'>TODO APP</div>
+            <div className='add-task'>
+                <AddTask addTask={addTask} />
+            </div>
+            <div className='list-tasks'>
+                {tasks.map((task,index) =>(
+                      <ListTask task={task}  removeTask={removeTask}
+                      index={index} key={index}/>
+                
+                ))}
+              
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default Todo;
